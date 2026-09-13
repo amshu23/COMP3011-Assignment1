06507808a81d339bd85b8e524921a01e3c289136
amshu23 <amshula.ashokbabu@student.adelaide.edu.au>
2026-09-13 17:11:20 +0930

    added the transcription service structure

--- src/main/java/comp3011/assignment1/controller/SpeechController.java
+++ src/main/java/comp3011/assignment1/controller/SpeechController.java
@@ -14,6 +14,6 @@ public class SpeechController {
     
     @GetMapping("/api/test")
     public String test() {
-        	return "API is working!!";
+        	return transcriptionService.checkApiKey();
     }
 }
--- src/main/java/comp3011/assignment1/service/TranscriptionService.java
+++ src/main/java/comp3011/assignment1/service/TranscriptionService.java
@@ -1,13 +1,24 @@
 package comp3011.assignment1.service;
 import comp3011.assignment1.config.OpenAiConfig;
 import org.springframework.stereotype.Service;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class TranscriptionService {
 	private final OpenAiConfig openAiConfig;
+	private final HttpClient httpClient;
+	
 	public TranscriptionService(OpenAiConfig openAiConfig)
 	{
 		this.openAiConfig = openAiConfig;
+		this.httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(10)).build();
 	}
 	
 	public String checkApiKey()
